b8681f3dcb553d1af86bd78a4c07419e0db72980
Hasanat902 <abulhasanat902@gmail.com>
2022-04-12 00:52:15 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Footer = () => {
+
+    const today = new Date();
+    const year = today.getFullYear();
+
     return (
-        <div>
-            <p>Copyright</p>
+        <div className='text-center my-5 text-black'>
+            <p>Copyright  {year}  by Md Abul Hasanat</p>
         </div>
     );
 };
